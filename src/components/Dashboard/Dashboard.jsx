5518dc33539065fb82429796dd6fa0ad8009c00e
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-13 18:06:06 +0600

    delete dashbord css

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ import React, { createContext } from 'react';
 import Barchart from '../Barchart/Barchart';
 import PiechartDashboard from '../Piechart/PiechartDashboard';
 import { Col, Row } from 'react-bootstrap';
-// import './Dashboard.css';
 
 
 const Dashboard = () => {
@@ -11,12 +10,12 @@ const Dashboard = () => {
             <h1 className='bg-info py-5 ps-5 text-Black'>Dashboard</h1>
             <Row xs={1} md={2}>
                 <Col>
-                <h1>Course Enrollment</h1>
-                <Barchart></Barchart>
+                    <h1>Course Enrollment</h1>
+                    <Barchart></Barchart>
                 </Col>
                 <Col>
-                <h1>Gender</h1>
-                <PiechartDashboard></PiechartDashboard>
+                    <h1>Gender</h1>
+                    <PiechartDashboard></PiechartDashboard>
                 </Col>
             </Row>
         </div>
