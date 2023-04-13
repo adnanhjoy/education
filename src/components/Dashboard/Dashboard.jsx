5d705b6057679ea237e5aa9a0be9b8c999710862
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-13 16:48:35 +0600

    responsive dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,23 +1,24 @@
 import React, { createContext } from 'react';
 import Barchart from '../Barchart/Barchart';
 import PiechartDashboard from '../Piechart/PiechartDashboard';
-import './Dashboard.css';
+import { Col, Row } from 'react-bootstrap';
+// import './Dashboard.css';
 
 
 const Dashboard = () => {
     return (
         <div>
-            <h1>This is Dashboard page</h1>
-            <div className='recharts-container'>
-                <div>
-                    <h1>Course Enrollment</h1>
-                    <Barchart></Barchart>
-                </div>
-                <div>
-                    <h1>Gender</h1>
-                    <PiechartDashboard></PiechartDashboard>
-                </div>
-            </div>
+            <h1 className='bg-info py-5 ps-5 text-Black'>Dashboard</h1>
+            <Row xs={1} md={2}>
+                <Col>
+                <h1>Course Enrollment</h1>
+                <Barchart></Barchart>
+                </Col>
+                <Col>
+                <h1>Gender</h1>
+                <PiechartDashboard></PiechartDashboard>
+                </Col>
+            </Row>
         </div>
     );
 };
